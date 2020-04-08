db73c3cff0e08fa3f723a019185bc092e25ec2e9
Daniel Atwood <datwood@riseup.net>
2020-04-08 17:55:52 +0200

    Make all tests pass again.

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from roll.testing import Client as BaseClient
 from copanier import app as copanier_app
 from copanier import config as kconfig
 from copanier.utils import create_token
-from copanier.models import Delivery, Person, Product
+from copanier.models import Delivery, Person, Product, Producer, Groups, Group
 
 
 def pytest_configure(config):
@@ -25,7 +25,7 @@ def pytest_runtest_setup(item):
 
 
 class Client(BaseClient):
-    content_type = 'application/x-www-form-urlencoded; charset=utf-8'
+    content_type = "application/x-www-form-urlencoded; charset=utf-8"
     headers = {}
 
     async def request(
@@ -67,12 +67,44 @@ def app():  # Requested by Roll testing utilities.
 @pytest.fixture
 def delivery():
     return Delivery(
-        name="Andines",
+        name="CRAC d'automne",
         contact="mister@me.me",
         from_date=datetime.now() + timedelta(days=10),
         to_date=datetime.now() + timedelta(days=10),
         order_before=datetime.now() + timedelta(days=7),
-        products=[Product(name="Lait", ref="123", price=1.5)],
+        products=[
+            Product(name="Lait", producer="ferme-du-coin", ref="lait", price=1.5,)
+        ],
+        producers={"ferme-du-coin": Producer(name="Ferme du coin", id="ferme-du-coin")},
+    )
+
+
+@pytest.fixture
+def groups():
+    fractal_brocolis = Group(
+        id="fractal-brocolis", name="The Fractal Brocolis", members=["foo@bar.org"]
+    )
+    groups = Groups({"fractal-brocolis": fractal_brocolis})
+    groups.persist()
+    return groups
+
+
+@pytest.fixture
+def yaourt():
+    return Product(
+        ref="yaourt",
+        unit="pot 125ml",
+        name="Yaourt",
+        price="3.5",
+        packing=4,
+        producer="ferme-du-coin",
+    )
+
+
+@pytest.fixture
+def fromage():
+    return Product(
+        ref="fromage", name="Fromage", price="9.2", producer="ferme-du-coin",
     )
 
 
--- tests/test_reports.py
+++ tests/test_reports.py
@@ -6,20 +6,18 @@ from copanier import reports
 from copanier.models import Order, Product, ProductOrder
 
 
-def test_summary_report(delivery):
+def test_summary_report(delivery, yaourt, fromage):
     delivery.products[0].packing = 6
-    delivery.products.append(
-        Product(ref="456", name="yaourt", price="3.5", packing=4, unit="pot 125ml")
-    )
-    delivery.products.append(Product(ref="789", name="fromage", price="9.2"))
-    delivery.orders["foo@bar.org"] = Order(
-        products={"123": ProductOrder(wanted=1), "456": ProductOrder(wanted=4)}
+    delivery.products.append(yaourt)
+    delivery.products.append(fromage)
+    delivery.orders["fractals-brocoli"] = Order(
+        products={"lait": ProductOrder(wanted=1), "yaourt": ProductOrder(wanted=4)}
     )
     delivery.persist()
     wb = load_workbook(filename=BytesIO(reports.summary(delivery)))
     assert list(wb.active.values) == [
         ("ref", "produit", "prix unitaire", "quantité commandée", "unité", "total"),
-        ("123", "Lait", 1.5, 1, None, 1.5),
-        ("456", "yaourt (pot 125ml)", 3.5, 4, "pot 125ml", 14),
+        ("lait", "Lait", 1.5, 1, None, 1.5),
+        ("yaourt", "Yaourt", 3.5, 4, "pot 125ml", 14),
         (None, None, None, None, "Total", 15.5),
     ]
